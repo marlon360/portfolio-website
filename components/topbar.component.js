e8e465b608ccb4a92d67fdeadfd0312031811390
Marlon Lückert <m.lueckert@me.com>
2020-04-30 21:15:47 +0200

    theme switcher

--- components/topbar.component.js
+++ components/topbar.component.js
@@ -1,11 +1,23 @@
 import styles from './topbar.module.css';
 
 export default function TopbarComponent() {
+
+    const onChange = (evt) => {
+        document.body.setAttribute('data-theme', evt.target.value);
+    }
+
     return (
         <div className={styles.topbar}>
             <div className={styles["topbar-content"]}>
                 <span>Marlon Lückert</span>
-                <a href="mailto:m.lueckert@me.com">m.lueckert@me.com</a>
+                <div className="theme-switcher">
+                    <label for="theme-switcher">Theme</label>
+                    <select id="theme-switcher" onChange={onChange} className={styles.select}>
+                        <option value="system">System</option>
+                        <option value="light">Light</option>
+                        <option value="dark">Dark</option>
+                    </select>
+                </div>
             </div>
         </div>
     )

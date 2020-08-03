0c15877154b8394a669a48be2a13a3f63ac22cda
Marlon Lückert <m.lueckert@me.com>
2020-08-03 14:01:29 +0200

    added google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,23 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-41295198-6"
+          />
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'UA-41295198-6');
+                `,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
